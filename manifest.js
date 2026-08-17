a38a4f777a64e98324d34e4cd676b166bbfd6922
nsnamikaze93-bit <nsnamikaze93@gmail.com>
2026-08-17 09:56:15 +0200

    Renombrado a Kuroneko

--- manifest.js
+++ manifest.js
@@ -1,7 +1,7 @@
 const manifest = {
-  id: 'net.jkanime.stremio',
+  id: 'net.kuroneko.stremio',
   version: '1.0.0',
-  name: 'JKanime',
+  name: 'Kuroneko',
   description:
     'Streams de anime desde JKanime.net (Sub Español y Latino) y AnimeJara (Castellano y Japonés sub). Usa IDs IMDB (tt...) o Kitsu (kitsu:ID).',
   types: ['series', 'movie'],
